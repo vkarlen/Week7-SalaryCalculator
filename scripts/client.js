ab9051db234d4e9620d85ce65d8a6954d50dddef
Vada Karlen <vada.karlen@gmail.com>
2021-01-29 16:42:01 -0600

    Connected wiring on inputs and buttons. INputs push to employeeList array.

--- scripts/client.js
+++ scripts/client.js
@@ -2,6 +2,31 @@ console.log('in js');
 
 $(document).ready(onReady);
 
+const employeeList = [];
+
 function onReady() {
   console.log('lets do this');
+
+  $('#submitButton').on('click', addEmployee);
+  $(document).on('click', '.deleteButton', deleteEmployee);
+}
+
+function addEmployee(e) {
+  e.preventDefault();
+
+  console.log('in add');
+
+  const newEmployee = {
+    FirstName: $('#fnInput').val(),
+    LastName: $('#lnInput').val(),
+    IDNum: Number($('#idInput').val()),
+    Title: $('#titleInput').val(),
+    Salary: Number($('#salaryInput').val()),
+  };
+
+  employeeList.push(newEmployee);
+}
+
+function deleteEmployee(params) {
+  console.log('in delete');
 }
